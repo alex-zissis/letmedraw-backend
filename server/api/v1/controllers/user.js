dea85f0666d7ed29fb3215eeca0fa71ebb162718
John Taylor <John@johns-macbook-pro.local>
2019-11-07 20:48:10 -0500

    added some user routes

--- server/api/v1/controllers/user.js
+++ server/api/v1/controllers/user.js
@@ -1,3 +1,15 @@
 exports.hello = async (req, res, next) => {
     res.status(200).json({ 'status': 'hello' });
 };
+
+exports.get_user = async (req, res, next) => {
+    // SELECT user_id, firstName, surname FROM users WHERE user_id = ${req.params.userId}
+    res.status(200).json({ userId: req.params.user_id, firstName: 'Alex', lastName: 'Zissis' });
+}
+
+exports.new_user = async (req, res, next) => {
+    console.log(req.body);
+    // store that in the data
+    // INSERT INTO users (firstName, lastName) VALUES (${req.body.firstName}, ${req.body.lastName});
+    res.status(200).json({status: 'success'});
+}
--- server/api/v1/routes/user.js
+++ server/api/v1/routes/user.js
@@ -3,4 +3,7 @@ const UsersController = require('../controllers/user');
 
 router.get('/hello', UsersController.hello);
 
+router.get('/:user_id', UsersController.get_user);
+
+router.post('/:user_id', UsersController.new_user);
 module.exports = router;
