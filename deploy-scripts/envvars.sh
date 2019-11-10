2ff08a6781f17dd0eab6c1511ab3e3604120a156
Alex Zissis <alex@alexzissis.tech>
2019-11-09 20:48:43 -0500

    fixed travis branch logic

--- deploy-scripts/envvars.sh
+++ deploy-scripts/envvars.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ $TRAVIS_BRANCH = "MASTER" ]; then
+if [ $TRAVIS_BRANCH == "master" ]; then
     echo "export ENVIRONMENT=prod" > .env
 else 
     echo "export ENVIRONMENT=staging" > .env
